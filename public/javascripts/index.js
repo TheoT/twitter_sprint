f99909c269ca058c352fe51a5c3dd77335512930
Theo Thompson <theodore.thompson@students.olin.edu>
2013-03-26 15:27:23 -0400

    adding public folder

---
+++ public/javascripts/index.js
@@ -0,0 +1,14 @@
+$(function (){
+	var data = {
+	labels : ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+	datasets : [
+		{
+			fillColor : "rgba(4,69,133,.85)",
+			strokeColor : "rgba(0,0,120,1)",
+			data : [.12,.15,.19,.11,-.06]
+		}
+	]
+}
+	var ctx = document.getElementById("tweetChart").getContext("2d");
+	var myNewChart = new Chart(ctx).Line(data);
+})
