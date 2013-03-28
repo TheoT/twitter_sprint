71b83b2aaa4f27cee520418ca09c0c0ce1867ef5
abekim <abekim0607@gmail.com>
2013-03-28 15:56:33 -0400

    cleaned up app, added .env to gitignore

--- app.js
+++ app.js
@@ -37,90 +37,6 @@
 // //   console.log("Express server listening on port " + app.get('port'));
 // // });
 
-// /**
-//  * Setup Twitter.
-//  */
-
-// console.log('accessing twitter');
-
-// var twitter = rem.connect('twitter.com').configure({
-//   key: process.env.TWITTER_KEY,
-//   secret: process.env.TWITTER_SECRET
-// });
-
-
-// var oauth = rem.oauth(twitter, 'http://' + app.get('host') + '/oauth/callback');
-
-// app.get('/login/', oauth.login());
-
-// app.use(oauth.middleware(function (req, res, next) {
-//   console.log("The user is now authenticated.");
-//   res.redirect('/');
-// }));
-
-// app.get('/logout/', oauth.logout(function (req, res) {
-//   res.redirect('/');
-// }));
-
-// // Save the user session as req.user.
-// app.all('/*', function (req, res, next) {
-//   req.api = oauth.session(req);
-//   next();
-// });
-
-// /**
-//  * Routes
-//  */
-
-// function loginRequired (req, res, next) {
-//   if (!req.api) {
-//     res.redirect('/login/');
-//   } else {
-//     next();
-//   }
-// }
-
-// app.get('/', loginRequired, function (req, res) {
-//   req.api('account/verify_credentials').get(function (err, profile) {
-//     res.send('Hi ' + profile.screen_name + '! <form action="/status" method="post"><input name="status"><button>Post Status</button></form>');
-//   });
-// });
-
-// app.post('/status', loginRequired, function (req, res) {
-//   req.api('statuses/update').post({
-//     status: req.body.status
-//   }, function (err, json) {
-//     if (err) {
-//       res.json({error: err});
-//     } else {
-//       res.redirect('http://twitter.com/' + json.user.screen_name + '/status/' + json.id_str);
-//     }
-//   });
-// })
-
-// app.listen(app.get('port'), function () {
-//   console.log('Listening on http://' + app.get('host'))
-// });
-
-// /**
-//  * Streaming example
-//  */
-
-// var carrier = require('carrier');
-
-// app.get('/stream', loginRequired, function (req, res) {
-//   console.log(req.api);
-//   req.api.stream('statuses/filter').post({
-//     track: ['obama', 'usa']
-//   }, function (err, stream) {
-//     carrier.carry(stream, function (line) {
-//       var line = JSON.parse(line);
-//       console.log(line.text + '\n');
-//     });
-//   });
-// })
-
-
 
 // npm install express rem
 var rem = require('rem')
@@ -252,7 +168,7 @@ app.get('/', loginRequired, function (req, res) {
       }
       var dateCreated = stats[0].created_at;
       newDate = dateCreated.substring(26) + '-' + months[dateCreated.substring(4, 7)] + '-' + dateCreated.substring(8, 10)
-      
+
       tws[newDate] = msg;
 
       console.log(tws);
